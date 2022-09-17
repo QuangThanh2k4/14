d02970bbfb8dbc77b99b5ed4388551e43dc70621
QuangThanh2k4 <nguyenquang101104@gmailcom>
2022-09-17 15:26:38 +0700

    gửi lên GitHub

---
+++ TableView/CustomTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTableViewCell.swift
+//  TableView
+//
+//  Created by Quang Thanh on 15/09/2022.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var iconImageView: UIImageView!
+    
+    @IBOutlet weak var tittleLabel01: UILabel!
+    
+    }
+
+
--- TableView/ViewController.swift
+++ TableView/ViewController.swift
@@ -7,13 +7,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var table: UITableView!
+    
+    struct Sunset {
+        let title: String
+        let imageName: String
+      
+    }
+    let data: [Sunset] = [
+        Sunset(title: "Little Creatures - Club Street", imageName: "food1"),
+        Sunset(title: "Yanti Nasi Padang", imageName: "food2"),
+        Sunset(title: "Tiong Bahru Bakery", imageName: "food3"),
+        Sunset(title: "Kitchen by Food Rebel" , imageName: "food4"),
+    ]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        table.dataSource = self
+        table.delegate = self
+        
     }
-
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) ->
+    UITableViewCell {
+        let sunset = data[indexPath.row]
+        let cell = table.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as!
+        CustomTableViewCell
+        cell.tittleLabel01.text = sunset.title
+        cell.iconImageView.image = UIImage(named: sunset.imageName)
+        return cell
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) ->
+    CGFloat {
+        return 140
+    }
 }
+
+
+
+
+
 
